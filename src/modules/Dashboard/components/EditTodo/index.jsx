a4b52bdc632bdff4e6f58a9bb53eabce63fcff59
Oleksandr Zinevych <aleksanderzinewicz@gmail.com>
2021-05-13 00:49:58 +0300

    next step of form creation

--- src/modules/Dashboard/components/EditTodo/index.jsx
+++ src/modules/Dashboard/components/EditTodo/index.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
@@ -5,33 +7,84 @@ import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 
 import PropTypes from "prop-types";
 
-const EditTodo = ({ open, handleClose }) => (
-  <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-    <DialogTitle id="form-dialog-title">Add</DialogTitle>
-    <DialogContent>
-      <DialogContentText>Add/Update todo item</DialogContentText>
-      <TextField
-        autoFocus
-        margin="dense"
-        id="title"
-        label="Title"
-        type="text"
-        fullWidth
-      />
-    </DialogContent>
-    <DialogActions>
-      <Button onClick={handleClose} color="primary">
-        Cancel
-      </Button>
-      <Button onClick={handleClose} color="primary">
-        Create
-      </Button>
-    </DialogActions>
-  </Dialog>
-);
+const EditTodo = ({ open, handleClose }) => {
+  const [state, setState] = useState({
+    type: "",
+  });
+
+  const handleChange = (event) => {
+    const { name } = event.target;
+    setState({
+      ...state,
+      [name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    handleClose();
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="form-dialog-title"
+    >
+      <form onSubmit={handleSubmit}>
+        <DialogTitle id="form-dialog-title">Add</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Add/Update todo item</DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="title"
+            label="Title"
+            type="text"
+            fullWidth
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="desc"
+            label="Description"
+            type="text"
+            fullWidth
+          />
+          <InputLabel htmlFor="age-native-simple">Type</InputLabel>
+          <Select
+            native
+            value={state.type}
+            onChange={handleChange}
+            inputProps={{
+              name: "age",
+              id: "age-native-simple",
+            }}
+          >
+            <option aria-label="None" value="" />
+            <option value="Personal">Personal</option>
+            <option value="Work">Work</option>
+            <option value="Study">Study</option>
+          </Select>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button type="submit" color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+};
 
 EditTodo.propTypes = {
   open: PropTypes.bool.isRequired,
